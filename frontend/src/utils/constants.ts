e1ae59ff67fcda0aefd11a29df50bf59eedc7725
baonv <baonv@kozo-japan.com>
2023-01-16 14:29:59 +0700

    update server

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'ws://note-app-server-eznl.onrender.com';
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://note-app-server-eznl.onrender.com';
 export const GRAPHQL_SERVER = 'https://note-app-server-eznl.onrender.com';
